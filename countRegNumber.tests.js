3b9e4ab60ce45aa3e99853ece3fc682a69895e57
Sesethu24 <seshmomoza20@gmail.com>
2019-05-17 15:35:12 +0200

    3rd commit

--- countRegNumber.tests.js
+++ countRegNumber.tests.js
@@ -3,5 +3,8 @@ describe('countRegNumber' , function(){
 
         assert.deepEqual(countRegNumber('CA 182736,CY 523519,CJ 812328'),(3));
     });
+    it('should return number of registration numbers in the string' , function(){
 
+        assert.deepEqual(countRegNumber('CY 523519'),(1));
+    });
 });
